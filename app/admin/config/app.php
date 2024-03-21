656693e8d4a9f593e7d71c0ee794e23bcd882b3d
handengke <17600712375@163.com>
2024-03-21 13:21:32 +0800

    666

--- app/admin/config/app.php
+++ app/admin/config/app.php
@@ -1,10 +1,23 @@
 <?php
+/*
+ * @Descripttion: 
+ * @Author: yang fu ren
+ * @version: 
+ * @Date: 2024-03-21 11:24:41
+ * @LastEditors: yang fu ren
+ * @LastEditTime: 2024-03-21 11:26:01
+ */
 // +----------------------------------------------------------------------
 // | 应用设置
 // +----------------------------------------------------------------------
 
 return [
     // 默认跳转页面对应的模板文件
+
+    
     'dispatch_success_tmpl' => app()->getAppPath() . '/tpl/dispatch_jump.tpl',
     'dispatch_error_tmpl'  => app()->getAppPath() . '/tpl/dispatch_jump.tpl',
 ];
+
+
+
